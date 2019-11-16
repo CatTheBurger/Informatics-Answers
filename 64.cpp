44e17a0ba4d1d69a7fb6be510e5ecc77b8f6a01b
ArtemY <35269498+CatTheBurger@users.noreply.github.com>
2019-11-16 19:24:41 +0300

    Create 64.cpp

---
+++ 64.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+    int size;
+
+    cin >> size;
+
+    int arr[size];
+
+    for(int i = 0;i < size;i++) {
+        cin >> arr[i];
+    }
+
+    for(int i = 0;i < size;i++) {
+        if(arr[i] % 2 == 0) cout << arr[i] << " ";
+    }
+
+    cout << endl;
+
+    return 0;
+}
